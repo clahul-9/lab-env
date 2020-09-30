c30d2a69485c4c18a80978df10d2f920b532c8c3
Clara Hultén <clahul-9@student.ltu.se>
2020-09-30 11:26:16 +0200

    14

--- build/projects/Mesh_resources/MeshResource.cpp
+++ build/projects/Mesh_resources/MeshResource.cpp
@@ -1,6 +1,10 @@
 #include "MeshResource.h"
+#include "GL/glew.h"
 
-
-void MeshResource::vertexBuffer() {
-
+void MeshResource::vertexBuffer(vertex v[], int size) {
+	float *buf = new float[size];
+	buf[]=(float) v[];
+	
+	GLuint vbo;
+	glGenBuffers(1, &vbo);
 }
--- build/projects/Mesh_resources/MeshResource.h
+++ build/projects/Mesh_resources/MeshResource.h
@@ -1,6 +1,14 @@
 #pragma once
 class MeshResource
 {
-	void vertexBuffer();
+	struct vertex
+	{
+		float position[3];
+		float color[4];
+	};
+
+	MeshResource();
+
+	void vertexBuffer(vertex v[], int size);
 };
 
